5a9f07f96de9f7492d5a0ca50cac2d24a31cd567
MirkoUnibo <mirko.scozia@studio.unibo.it>
2023-08-22 18:07:17 +0200

    refactor login

--- src/main/java/com/example/msbolopoint/dto/SignUpDto.java
+++ src/main/java/com/example/msbolopoint/dto/SignUpDto.java
@@ -1,8 +1,14 @@
 package com.example.msbolopoint.dto;
 
-import lombok.Data;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
-@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
 public class SignUpDto {
     private String name;
     private String username;
